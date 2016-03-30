e62065d53ddba5344240c5bb98de83f1c78c5342
AkiraPanda <akirapanda@newnil.com>
2016-03-30 13:28:48 +0800

    page26 finished

--- spec/feature/accounts/sign_up_spec.rb
+++ spec/feature/accounts/sign_up_spec.rb
@@ -6,6 +6,7 @@ feature "Account" do
     click_link "Account Sign Up"
     
     fill_in  'Name', :with => "Test"
+    fill_in  'Subdomain', :with => "Test"
     fill_in  'Email',:with => "subscribem@example.com"
     fill_in  'Password', :with=> "password"
     fill_in  'Password confirmation', :with => "password"
@@ -13,5 +14,6 @@ feature "Account" do
     click_button "Create Account"
     success_message = "Signed in as subscribem@example.com."
     expect(page).to have_content(success_message)
+    expect(page.current_url).to eq("http://test.example.com/subscribem/")
   end
 end
